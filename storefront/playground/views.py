23cbf3dfd167f078459f6a80859888714d2574c8
vikmaamaa <maamaavictor@yahoo.com>
2023-03-19 09:41:15 +0100

    feat: deffering fields

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.db.models import Q, F
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 
-from store.models import Product
+from store.models import Product, OrderItem
 # Create your views here.
 
 def calculate():
@@ -11,9 +12,15 @@ def calculate():
     return x
 
 def say_hello(request):
+    # order = OrderItem.objects.all()
+    # pro = []
+    # for a in order:
+    #     print(a.product_id)
+    #     t = Product.objects.filter(pk=a.product_id).first()
+    #     pro.append(t)
+    # exists = Product.objects.values('id', 'title', 'collection__title')
     
-    exists = Product.objects.filter(pk=1).exists()
+    exists = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
     
-    
-    
-    # return render(request, 'hello.html', {'name': 'Mosh'})
+    # print(exists)
+    return render(request, 'hello.html', {'name': 'Mosh', 'products':list(exists)})
