d73e938ac5ddad810a6b7b881dbc2b0b2642b8f1
vikmaamaa <maamaavictor@yahoo.com>
2023-03-19 17:51:08 +0100

    feat: custom managers

--- storefront/tags/views.py
+++ storefront/tags/views.py
@@ -5,13 +5,6 @@ from tags.models import TaggedItem
 
 # Create your views here.
 def say_hello(request):
-    content_type = ContentType.objects.get_for_models(Product)
-    
-    queryset = TaggedItem.objects \
-    .select_related('tag') \
-    .filter(
-        content_type=content_type,
-        object_id = 1
-    )
+    TaggedItem.objects.get_tags_for(Product, 1)
     
     return render(request, 'hello.html', {'name': 'Mosh', 'tags': list(queryset)})
