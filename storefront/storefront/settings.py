a2059487cba3e97f8f3fd30bc1f9c3092a99d66b
vikmaamaa <maamaavictor@yahoo.com>
2023-03-19 03:17:49 +0100

    feat: using mysql in django

--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -87,9 +87,16 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'aeiou08037884294'
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
