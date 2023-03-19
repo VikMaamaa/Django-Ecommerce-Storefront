6d97c5b2bc7619cbcfcab42766c88bfa766cd60b
vikmaamaa <maamaavictor@yahoo.com>
2023-03-19 21:22:20 +0100

    feat: selecting related objects

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from .models import Collection, Product, Customer
+from .models import Collection, Product, Customer, Order
 # Register your models here.
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+    
+    def collection_title(self, product):
+        return product.collection.title
     
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
@@ -21,6 +25,10 @@ class CustomerAdmin(admin.ModelAdmin):
     ordering = ['first_name', 'last_name'] 
     list_per_page = 10
     
+  
+@admin.register(Order)    
+class OrderAdmin(admin.ModelAdmin):    
+    list_display = ['id', 'placed_at', 'customer']
     
 admin.site.register(Collection)
 # admin.site.register(Product)
